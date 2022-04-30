eddc02ba1f1d82df5daf0f28aab1bd6029a7cadf
Joao Guis <joao.guis2001@gmail.com>
2022-04-29 22:19:03 -0300

    trying to use emotion as styles lib

---
+++ src/doeJaClasses.ts
@@ -0,0 +1,11 @@
+import { css } from '@emotion/react'
+
+const doeJaClasses = {
+  blankPageContainer: css({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }),
+}
+
+export default doeJaClasses
---
+++ src/login/Login.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Box, Grid, Paper } from '@mui/material'
+import doeJaClasses from '../doeJaClasses'
+
+function Login() {
+  return (
+    <Box css={doeJaClasses.blankPageContainer}>
+      <Paper>
+        <Grid container spacing={2}>
+          <Grid item />
+        </Grid>
+      </Paper>
+    </Box>
+  )
+}
+
+export default Login
